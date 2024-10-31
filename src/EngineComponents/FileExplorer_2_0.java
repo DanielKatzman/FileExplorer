08b282868c915b0c4baa15c16b5a831e4439b1f8
Daniel <daniel.katzman@s.afeka.ac.il>
2024-10-31 20:15:26 +0200

    changed the file explorer class 2.0 to match the explorer class!

--- src/EngineComponents/FileExplorer_2_0.java
+++ src/EngineComponents/FileExplorer_2_0.java
@@ -1,31 +1,38 @@
 package EngineComponents;
 
-import exceptions.FilesExceptions;
-import exceptions.FilesIsEmpty;
 import io.Gui;
 import io.Input;
 import java.io.File;
 import java.util.Optional;
 
 public class FileExplorer_2_0 extends Explorer{
-    private File chosenFile;
+//    private File chosenFile;
 
     public FileExplorer_2_0(){
         super(0); //no limit on depth!
     }
 
+
+
     public Optional<File> initiate(){
         File[] files = File.listRoots();
         Gui.selectSystemDisk(files);
         int input = Input.correctInput(files.length - 1);
-        File currentFile = files[input];
-        return explorer(currentFile);
+        currentDirectory = files[input];
+        return loop(currentDirectory);
     }
 
     @Override
-    protected Optional<File> handleNoMoreFiles(File currentFile){
-        System.out.printf("No more files in Directory! returning to Directory: %s %n", chosenFile.getParentFile().getName());
-        return explorer(currentFile.getParentFile());
+    protected Optional<File> loop(File currentFile) {
+        handleDirectory(currentFile);
+        handleReturn(currentFile);
+        return Optional.of(currentFile);
+    }
+
+    @Override
+    protected Optional<File> handleEmptyDirectory(File currentFile){
+        System.out.printf("No more files in Directory! returning to Directory: %s %n", currentDirectory.getParentFile().getName());
+        return loop(currentFile.getParentFile());
     }
 
     @Override
@@ -37,19 +44,19 @@ public class FileExplorer_2_0 extends Explorer{
 
         return switch (inputChoice) {
             case 0 -> Optional.empty(); //Exit statement
-            case 1 -> Optional.of(chosenFile); //choose the file
-            case 2 -> explorer(chosenFile); //Enter folder
-            case 3 -> explorer(currentFile.getParentFile()); //return to previous folder
+            case 1 -> Optional.of(currentDirectory); //choose the file
+            case 2 -> loop(currentDirectory); //Enter folder
+            case 3 -> loop(currentFile.getParentFile()); //return to previous folder
             default -> {
                 System.out.println("Invalid input");
-                yield explorer(currentFile);
+                yield loop(currentFile);
             }
         };
     }
 
     public Optional<File> handleNoAccess(){
-        System.out.printf("No access to current directory! returning to Directory: %s %n", chosenFile.getParentFile().getName());
-        return explorer(chosenFile.getParentFile());
+        System.out.printf("No access to current directory! returning to Directory: %s %n", currentDirectory.getParentFile().getName());
+        return loop(currentDirectory.getParentFile());
     }
 
     protected void handleDirectory(File currentFile){
@@ -57,7 +64,7 @@ public class FileExplorer_2_0 extends Explorer{
         Gui.printDirectoryDetails(files);
         Gui.chooseFileFromList();
         int inputFile = Input.correctInput(files.length - 1);
-        chosenFile = files[inputFile];
-        Gui.selectedDirectoyMessage(chosenFile);
+        currentDirectory = files[inputFile];
+        Gui.selectedDirectoyMessage(currentDirectory);
     }
 }
